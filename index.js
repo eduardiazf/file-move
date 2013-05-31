fb13026ae8f0b9c6b87036da1343a35ae4511a72
Eduardo Diaz <eduar.diaz37@gmail.com>
2013-05-31 14:11:01 -0400

    move file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var fs = require('fs'),
     alternate_move,
-    move;
+    moveFile;
 
 alternate_move = function (origin, destination, next) {
     var read  = fs.createReadStream(origin),
         write = fs.createWriteStream(destination);
 
     read.pipe(write);
+
     write.on('close', function () {
         fs.unlink(origin, function (err) {
             next(err);
@@ -14,11 +15,11 @@ alternate_move = function (origin, destination, next) {
     });
 };
 
-move = function (origin, destination, next) {
+moveFile = function (origin, destination, next) {
     fs.rename(origin, destination, function (err) {
         if (err) {return alternate_move(origin, destination, next);}
         next(null);
     });
 };
 
-module.exports = move;
+module.exports = moveFile;
